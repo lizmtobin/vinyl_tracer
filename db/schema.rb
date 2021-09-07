dd6719ef3ae6dbc86c41c634f4d0a4e8d4055f4b
liz tobin <lizmtobin@gmail.com>
2021-09-07 20:51:27 +0100

    added map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_105400) do
+ActiveRecord::Schema.define(version: 2021_09_07_180449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,8 @@ ActiveRecord::Schema.define(version: 2021_09_02_105400) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "seller_logo"
     t.string "map"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
