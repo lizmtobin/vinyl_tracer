19a9b338f60b8e99fbb7f14635149f545b66df75
liz tobin <lizmtobin@gmail.com>
2021-09-09 21:00:55 +0100

    map reappeared yay

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack');
+environment.loaders.delete("nodeModules");
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
