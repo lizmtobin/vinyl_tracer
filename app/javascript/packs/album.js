41c78661e5e04d775e36b352624b9064b41006ae
liz tobin <lizmtobin@gmail.com>
2021-09-11 12:15:27 +0100

    tinkering with container on album show

---
+++ app/javascript/packs/album.js
@@ -0,0 +1,23 @@
+      const artist = () => {
+          const artist_name = document.getElementById("artist_name");
+                    if(artist_name){
+                      $.ajax({
+                        url: 'http://en.wikipedia.org/w/api.php',
+                        data: { action: 'query', prop: 'extracts', redirects: 1, exsentences: 6, exintro: true, explaintext: true, titles: artist_name.innerText, format: 'json' },
+                        dataType: 'jsonp',
+                        success: processResult
+                    });
+            function processResult(apiResult){
+              const pages = apiResult.query.pages
+              const pages_inner = Object.keys(pages)[0]
+              const text = pages[pages_inner].extract
+            const artist_trivia = document.getElementById("artist_trivia");
+              artist_trivia.innerText = text;
+            }
+          }
+        }
+
+
+export {artist}
+
+//https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=rollingstones
