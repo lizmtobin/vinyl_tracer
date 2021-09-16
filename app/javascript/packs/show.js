7fd8521a7315022d6e56c50abef30ba8ee78c264
Gid Berridge <gid.berridge@gmail.com>
2021-09-16 12:37:02 +0100

    all

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,8 +1,46 @@
 const six = document.querySelector(".six-degrees");
 const js_tag = document.querySelector(".flag");
-// const js_tag2 = document.querySelector(".six-text")
+
 
 six.addEventListener("click", function() {
     js_tag.classList.toggle("hidden");
-    // js_tag2.classList.add("hidden")
+    fadeIn(el);
 });
+
+
+
+
+// fade out
+
+function fadeOut(el){
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+// fade in
+
+function fadeIn(el, display){
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .005) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+var el = document.querySelector('.js-fade');
+
+// fadeOut(el);
+// fadeIn(el);
+// fadeIn(el, "inline-block");
